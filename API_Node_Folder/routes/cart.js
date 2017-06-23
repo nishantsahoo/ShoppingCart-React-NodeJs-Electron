15bcee66d350fb70038520623e244aa1ca4bc964
Nishant Sahoo <nishantsahoo.in@gmail.com>
2017-06-23 15:17:22 +0530

    Cart API

--- API_Node_Folder/routes/cart.js
+++ API_Node_Folder/routes/cart.js
@@ -1,18 +1,41 @@
 const route = require('express').Router();
 const data = require('../mydata');
 
+// To get Cart items from the database
+route.get('/getcart', (req, res) => {
+    data.getCart().then((cart) => {
+    res.send(cart);
+})
+});
+
 // To add an item to the cart
 route.post('/addtocart', (req, res) => {
     data.addToCart(req.body.product);
 res.redirect('/myapi/mycart');
 });
 
-// to checkout from the cart
+// To checkout from the cart
 route.post('/checkout', (req, res) => {
     data.cartCheckout(req.body.name);
 res.redirect('/myapi/mycart');
 });
 
+// To decrease the quantity of a particuler Cart item by 1
+route.post('/decrementCart', (req, res) => {
+    data.decrementCart(req.body.id);
+res.redirect('/myapi/mycart');
+});
 
+// To increase the quantity of a particular Cart item by 1
+route.post('/incrementCart', (req, res) => {
+    data.incrementCart(req.body.id);
+res.redirect('/myapi/mycart');
+});
+
+// To delete a Cart item from the Cart (Database)
+route.post('/delfromcart', (req, res) => {
+    data.delFromCart(req.body.id);
+res.redirect('/myapi/mycart');
+});
 
 // this file is the route file for cart
--- API_Node_Folder/routes/products.js
+++ API_Node_Folder/routes/products.js
@@ -1,33 +1,13 @@
 const route = require('express').Router();
 const data = require('../mydata');
 
+// To get Products from the Database
 route.get('/', (req, res) => {
     data.getProducts().then((products) => {
     res.send(products);
 })
 });
 
-
-route.get('/getcart', (req, res) => {
-    data.getCart().then((cart) => {
-    res.send(cart);
-})
-});
-
-route.post('/decrementCart', (req, res) => {
-    data.decrementCart(req.body.id);
-res.redirect('/myapi/mycart');
-});
-
-route.post('/incrementCart', (req, res) => {
-    data.incrementCart(req.body.id);
-res.redirect('/myapi/mycart');
-});
-
-route.post('/delfromcart', (req, res) => {
-    data.delFromCart(req.body.id);
-res.redirect('/myapi/mycart');
-});
 
 route.get('/countproducts', (req, res) => {
     data.noofproducts().then((count) => {
