39e1f90fe65dd8b2e7727dccb82eeeed7a064c56
Nishant Sahoo <nishantsahoo.in@gmail.com>
2017-06-23 15:09:56 +0530

    Cors added to the server file

--- API_Node_Folder/server.js
+++ API_Node_Folder/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyparser = require('body-parser');
 const app = express(); // express object
+const cors=require('cors'); // to use cors
+
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended: true}));
 app.use('/static', express.static(__dirname + "/public_static"));
-
+app.use(cors());
 const ProductsRoute = require('./routes/products'); // route for products
 app.use('/myapi/products/', MyApiRoute);
 const CartRoute = require('./routes/cart'); // route for cart
